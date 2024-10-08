3703c73c4e257e87d3e8bfd64259f58f255437ae
aayushasthana <56047917+aayushasthana@users.noreply.github.com>
2024-10-07 21:05:12 -0500

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,9 @@
+import yfinance as yf
+
+# Example: Get financial data for a company
+ticker = 'AAPL'
+stock = yf.Ticker(ticker)
+financials = stock.financials
+balance_sheet = stock.balance_sheet
+cashflow = stock.cashflow
+print(cashflow)
